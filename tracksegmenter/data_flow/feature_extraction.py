83a5355dfdb535c03d72c5d230fc380075e00647
Aleksandr Kunin <skyksandr@gmail.com>
2018-01-13 15:56:37 +0300

    Fix pandas warnings

--- tracksegmenter/data_flow/feature_extraction.py
+++ tracksegmenter/data_flow/feature_extraction.py
@@ -1,8 +1,10 @@
-import pandas
 
 def extract_features(df):
     df['v_speed_change'] = df['v_speed'].pct_change().fillna(0).abs()
-    df['v_speed_change'] = pandas.rolling_median(df['v_speed_change'], window=3, center=True).fillna(0)
-
+    df['v_speed_change'] = (
+        df['v_speed_change'].rolling(window=3, center=True)
+                            .median()
+                            .fillna(0)
+    )
 
     return df
